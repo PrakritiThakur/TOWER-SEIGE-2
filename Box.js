3eae4145c3aab8d477de6be7d1d0e80da7ec6e77
PrakritiThakur <75303332+PrakritiThakur@users.noreply.github.com>
2021-03-20 19:29:37 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,27 @@
+  
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+
+    super(x,y,width,height);
+    this.visibility = 225;
+  }
+
+  display(){
+    if(this.body.speed < 3){
+    
+      var pos= this.body.position;
+      push();
+      translate(pos.x, pos.y);
+
+      rectMode(CENTER);
+      rect(0,0,this.width, this.height);
+      pop();
+    }
+    else{
+      World.remove(world, this.body);
+      push();
+      this.visibility = this.visibility -5;
+      pop();
+   }
+  }
+}
